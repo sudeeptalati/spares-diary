a67978507e192f0cd82e2f78836f8cae7ad9f430
kruthika.bethur@gmail.com <kruthika.bethur@gmail.com@ec17f337-a6db-d51d-ddb3-43e7fb620e18>
2012-03-06 14:50:04 +0000

    Export to Excel and CSV is working fine.

---
+++ trunk/stock_system/ims/protected/views/status/_form.php
@@ -0,0 +1,54 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'status-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textArea($model,'name',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'description'); ?>
+		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
+		<?php echo $form->error($model,'description'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created_by_user'); ?>
+		<?php echo $form->textField($model,'created_by_user'); ?>
+		<?php echo $form->error($model,'created_by_user'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'modified_by_user'); ?>
+		<?php echo $form->textField($model,'modified_by_user'); ?>
+		<?php echo $form->error($model,'modified_by_user'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'created'); ?>
+		<?php echo $form->textField($model,'created'); ?>
+		<?php echo $form->error($model,'created'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'modified'); ?>
+		<?php echo $form->textField($model,'modified'); ?>
+		<?php echo $form->error($model,'modified'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
